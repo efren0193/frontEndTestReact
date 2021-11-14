00177bc5a421537bf1672098cf6dd8346f528c48
efren <efren0193@gmail.com>
2021-11-13 19:02:47 -0600

    Add NotFound

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link}  from 'react-router-dom'
+import {
+    MDBContainer
+} from "mdb-react-ui-kit";
 
 const NotFound = () => {
     return(
-        'NotFound'
+        <main>
+           <MDBContainer size="md">
+                <section className="text-center my-5">
+                    <h2>Página no encontrada</h2>
+                    <hr/>
+                    <h5>Regresar a <Link to="/">Inicio</Link></h5>
+                </section>
+            </MDBContainer>
+        </main>
     )
 }
 
